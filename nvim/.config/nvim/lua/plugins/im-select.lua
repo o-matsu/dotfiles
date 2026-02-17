43d8c648c8452ffc726d903ee23c28c350fd1797
o-matsu <matsushita.ryosuke@pine-app.studio>
2026-02-18 01:52:08 +0900

    feat: :sparkles: Add im-select.nvim plugin configuration to Neovim setup

---
+++ nvim/.config/nvim/lua/plugins/im-select.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "keaising/im-select.nvim",
+    event = "VeryLazy",
+    opts = {
+      default_im_select = "com.apple.keylayout.ABC",
+      default_command = "macism",
+    },
+  },
+}
